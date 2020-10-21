09f7e17fa427af5ad4b395e7f0c52eb3c2d621f6
zainafzal-tek <zainafzal@gmail.com>
2020-10-21 01:16:20 -0400

    update Project

--- src/test/java/com/vytrack/pages/LoginPage.java
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.vytrack.pages;
 
+import com.vytrack.utils.ConfigurationReader;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,6 +18,33 @@ public class LoginPage extends BasePage {
     public void login(String usernameValue, String passwordValue){
         username.sendKeys(usernameValue);
         password.sendKeys(passwordValue, Keys.ENTER);
+    }
+
+    public void login(){
+        String usernameValue = ConfigurationReader.getProperty("storemanager.username");
+        String passwordValue = ConfigurationReader.getProperty("password");
+
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+
+    }
+
+    public void login(String role){
+
+        String usernameValue = "";
+        String passwordValue = ConfigurationReader.getProperty("password");
+
+        if (role.equalsIgnoreCase("sales manager")){
+            usernameValue = ConfigurationReader.getProperty("salesmanager.username");
+        } else if (role.equalsIgnoreCase("driver")){
+            usernameValue = ConfigurationReader.getProperty("driver.username");
+        } else {
+            usernameValue = ConfigurationReader.getProperty("storemanager.username");
+        }
+
+        username.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+
     }
 
 }
