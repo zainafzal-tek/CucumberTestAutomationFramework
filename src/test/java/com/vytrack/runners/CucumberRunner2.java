ec8c7cb86d6e147c9438f3c577df46e0889ac585
zainafzal-tek <zainafzal@gmail.com>
2020-11-12 21:38:29 -0500

    update project

---
+++ src/test/java/com/vytrack/runners/CucumberRunner2.java
@@ -0,0 +1,26 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "rerun:target/rerun.txt",
+                "json:target/cucumber2.json"
+        },
+
+        features = {
+                "src/test/resources/features/fleet"
+        },
+        glue = {"com/vytrack/step_definitions"},
+        dryRun = false,
+        publish = true,
+
+        //This is used to determine which tests cucumber would only run
+        tags = "@negative_login"
+)
+public class CucumberRunner2 {
+}
--- src/test/java/com/vytrack/utils/Driver.java
+++ src/test/java/com/vytrack/utils/Driver.java
@@ -14,67 +14,69 @@ public class Driver {
 
     private Driver(){}
 
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
     public static WebDriver getDriver(){
-        if (driver == null){
+        if (driverPool.get() == null) {
 
-            String browser = ConfigurationReader.getProperty("browser");
+            synchronized (Driver.class) {   //the code under this block will be thread safe meaning only one instance per thread
+                String browser = ConfigurationReader.getProperty("browser");
 
-            switch (browser){
+                switch (browser) {
 
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        driverPool.set(new ChromeDriver());
+                        break;
 
-                case "firefox":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
+                    case "firefox":
+                        WebDriverManager.chromedriver().setup();
+                        driverPool.set(new FirefoxDriver());
+                        break;
 
-                case "edge":
-                    WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
-                    break;
+                    case "edge":
+                        WebDriverManager.edgedriver().setup();
+                        driverPool.set(new EdgeDriver());
+                        break;
 
-                case "remote-chrome":
-                    try {
-                        //ChromeOptions chromeOptions = new ChromeOptions();
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName("chrome");
-                        URL gridUrl = new URL("http://18.205.113.72:4444/wd/hub");
-                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    break;
+                    case "remote-chrome":
+                        try {
+                            //ChromeOptions chromeOptions = new ChromeOptions();
+                            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                            desiredCapabilities.setBrowserName("chrome");
+                            URL gridUrl = new URL("http://18.205.113.72:4444/wd/hub");
+                            driverPool.set(new RemoteWebDriver(gridUrl, desiredCapabilities));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
 
-                case "remote-firefox":
-                    try {
-                        //FirefoxOptions firefoxOptions = new FirefoxOptions(); this object will replace
-                        //desired capabilities and set browser name line of code
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName("firefox");
-                        URL gridUrl = new URL("http://18.205.113.72:4444/wd/hub");
-                        driver = new RemoteWebDriver(gridUrl, desiredCapabilities);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    break;
+                    case "remote-firefox":
+                        try {
+                            //FirefoxOptions firefoxOptions = new FirefoxOptions(); this object will replace
+                            //desired capabilities and set browser name line of code
+                            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                            desiredCapabilities.setBrowserName("firefox");
+                            URL gridUrl = new URL("http://18.205.113.72:4444/wd/hub");
+                            driverPool.set(new RemoteWebDriver(gridUrl, desiredCapabilities));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
 
-                default:
-                    throw new RuntimeException("No such Browser yet@");
+                    default:
+                        throw new RuntimeException("No such Browser yet@");
+                }
+
             }
-
         }
-        return driver;
+        return driverPool.get();
     }
 
     public static void closeDriver(){
-        if (driver != null){
-            driver.quit();
-            driver = null;
+        if (driverPool.get() != null){
+            driverPool.get().quit();
+            driverPool.remove();
         }
     }
 
